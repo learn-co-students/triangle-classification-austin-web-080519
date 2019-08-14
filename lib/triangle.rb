f8d8dbcdbfb70ed3e5e49efb8678642cad1e7f42
andrewrosario <rosario.andrew@gmail.com>
2019-08-14 14:24:50 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    triangle = [@one, @two, @three].sort
+    if (triangle[0] + triangle[1] <= triangle[2]) || @one <= 0 || @two <= 0 || @three <= 0
+      puts "ERROR!!"
+      raise TriangleError
+    elsif triangle.sum/3.0 == @one
+      :equilateral
+    elsif (triangle.count{|tri| tri == @one} == 2) || (triangle.count{|tri| tri == @two} == 2)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Its not a triangle"
+    end
+  end
 end
